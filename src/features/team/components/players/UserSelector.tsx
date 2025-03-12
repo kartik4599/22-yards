ddff8b669cbf470259047969235ee81286afbe6c
kartik10-24 <kartik.mendu@mindinventory.com>
2025-03-12 18:16:06 +0530

    created user selection Component.

---
+++ src/features/team/components/players/UserSelector.tsx
@@ -0,0 +1,99 @@
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+import { X } from "lucide-react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { getUserList, User } from "../../api/fetch-player-team";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { getImageUrl } from "@/lib/database";
+
+const UserSelector = ({
+  setSelectedUser,
+  selectedUser,
+}: {
+  setSelectedUser: Dispatch<SetStateAction<User | null>>;
+  selectedUser: User | null;
+}) => {
+  const [userlist, setUserList] = useState<User[]>([]);
+  const [keyword, setKeyword] = useState("");
+
+  const fetchUserList = async () => {
+    const users = await getUserList(keyword);
+    setUserList(users);
+  };
+
+  useEffect(() => {
+    fetchUserList();
+  }, [keyword]);
+
+  return (
+    <Command className="rounded-lg border shadow-md min-w-[400px] md:min-w-[450px]">
+      <CommandInput
+        value={selectedUser?.name || keyword}
+        onValueChange={(value: string) => {
+          if (selectedUser) return;
+          setKeyword(value);
+        }}
+        placeholder="Search User"
+      />
+      {selectedUser && (
+        <CommandList>
+          <CommandGroup heading="Selected User">
+            <CommandItem
+              onSelect={setSelectedUser.bind(null, null)}
+              className="flex w-full justify-between items-center"
+            >
+              <div className="flex items-center gap-x-2">
+                <Avatar className="size-6">
+                  <AvatarImage
+                    src={getImageUrl({
+                      collectionId: selectedUser.collectionId,
+                      filename: selectedUser.avatar,
+                      id: selectedUser.id,
+                    })}
+                  />
+                </Avatar>
+                <span>{selectedUser.name}</span>
+              </div>
+              <X />
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      )}
+      <CommandSeparator />
+      {!selectedUser && (
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Users">
+            {userlist.map((user) => (
+              <CommandItem
+                onSelect={setSelectedUser.bind(null, user)}
+                key={user.id}
+                className="flex"
+              >
+                <Avatar className="size-6">
+                  <AvatarImage
+                    src={getImageUrl({
+                      collectionId: user.collectionId,
+                      filename: user.avatar,
+                      id: user.id,
+                    })}
+                  />
+                </Avatar>
+                <span>{user.name}</span>
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      )}
+    </Command>
+  );
+};
+
+export default UserSelector;
