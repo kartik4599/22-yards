84acbaa25b648052e03d8ecebce699d075e62f2d
kartik10-24 <kartik.mendu@mindinventory.com>
2025-03-12 14:00:51 +0530

    integrated in header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -11,8 +11,14 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { BiSolidCricketBall } from "react-icons/bi";
 import { MoblieSideBar } from "./SideBar";
+import { getImageUrl, getloggedInUser } from "@/lib/database";
+import { cookies } from "next/headers";
 
-const Header = () => {
+const Header = async () => {
+  const cookie = await cookies();
+  const { user } = getloggedInUser(cookie.get("pb_auth")?.value);
+  if (!user) return null;
+
   return (
     <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-background/5 backdrop-blur-xs px-4 md:px-6">
       <MoblieSideBar />
@@ -25,8 +31,15 @@ const Header = () => {
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" size="icon" className="rounded-full">
               <Avatar className="h-8 w-8">
-                <AvatarImage src="/placeholder-user.jpg" alt="User" />
-                <AvatarFallback>MS</AvatarFallback>
+                <AvatarImage
+                  src={getImageUrl({
+                    collectionId: user.collectionId,
+                    filename: user.avatar,
+                    id: user.id,
+                  })}
+                  alt="User"
+                />
+                <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
               </Avatar>
             </Button>
           </DropdownMenuTrigger>
